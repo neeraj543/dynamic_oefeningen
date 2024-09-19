87b73841cb5d610954105a189cc607f1195c3642
Neeraj <r1008394@student.thomasmore.be>
2024-09-19 10:17:32 +0200

    normalize.css installation

--- components/MenuProduct.jsx
+++ components/MenuProduct.jsx
@@ -1,9 +1,21 @@
 export function MenuProduct(props) {
-    const { product} = props;
-    product.size ??= "0";
+    const {product} = props;
+    if (!product?.name  )return ;
     return (
-        <div style ={{margin: "2vw", fontSize: "larger"}}>
-            {`${product.name} (${product.size} cl)  ${product.price} -- \u20AC`}
+        <div style={{margin: "2vw", fontSize: "larger", display: "flex"}}>
+            <div style={{flex: 1}}>
+                {product.name}
+                <div style={{color:"blue" ,     display:"inline"}}>
+                    {product.size ? ` (${product.size} cl)` : ''}
+                </div>
+                <div style={{color:"blue" , fontSize:14}}>
+                    {product.note}
+                </div>
+            </div>
+            <div style={{flex: 1}}>
+                {product.price} &euro;
+            </div>
         </div>
     );
 }
+// product.size ??= "";
